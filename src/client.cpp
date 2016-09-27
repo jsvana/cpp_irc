435fb33a8e56ec9b96237ff6dbd7097e90a7f696
Jay Vana <jsvana@mtu.edu>
2016-09-26 21:46:43 -0700

    Add callbacks, it connects!

--- src/client.cpp
+++ src/client.cpp
@@ -5,15 +5,16 @@
 void dispatcher(Client &client) {
   client.write_lines({
     "USER jsvana 0.0.0.0 0.0.0.0 :jsvana test",
-    "NICK jsvana",
+    "NICK " + client.nick_choices.front(),
   });
+  client.nick_choices.pop();
 
   while (true) {
     auto message = client.read();
 
     const auto &callbacks = client.callbacks_for_command(message.command());
     for (const auto &callback : callbacks) {
-      callback(client);
+      callback(client, message);
     }
   }
 }
@@ -22,15 +23,15 @@ Client::Client(const std::string &host, const std::string &port) {
   sock_ = std::make_unique<IrcSocket>(host, port);
 }
 
-void Client::add_callback(const std::string &command, const std::function<void(Client &)> callback) {
+void Client::add_callback(const std::string &command, const std::function<void(Client &, const Message &)> callback) {
   auto iter = callbacks_.find(command);
   if (iter == callbacks_.end()) {
-    callbacks_.emplace(std::make_pair(command, std::vector<std::function<void(Client &)>>()));
+    callbacks_.emplace(std::make_pair(command, std::vector<std::function<void(Client &, const Message &)>>()));
   }
   callbacks_[command].push_back(callback);
 }
 
-const std::vector<std::function<void(Client &)>> &Client::callbacks_for_command(const std::string &command) {
+const std::vector<std::function<void(Client &, const Message &)>> &Client::callbacks_for_command(const std::string &command) {
   auto iter = callbacks_.find(command);
   if (iter == callbacks_.end()) {
     return EMPTY_CALLBACKS;
--- src/client.h
+++ src/client.h
@@ -3,9 +3,11 @@
 #include "irc_socket.h"
 #include "message.h"
 
+#include <iostream>
 #include <functional>
 #include <map>
 #include <memory>
+#include <queue>
 #include <string>
 #include <thread>
 #include <vector>
@@ -13,16 +15,24 @@
 class Client {
  private:
   std::unique_ptr<IrcSocket> sock_;
-  std::map<std::string, std::vector<std::function<void(Client &)>>> callbacks_;
+  std::map<std::string, std::vector<std::function<void(Client &, const Message &)>>> callbacks_;
 
-  const std::vector<std::function<void(Client &)>> EMPTY_CALLBACKS;
+  const std::vector<std::function<void(Client &, const Message &)>> EMPTY_CALLBACKS;
 
  public:
+  std::queue<std::string> nick_choices;
+
   Client(const std::string &host, const std::string &port);
 
   IrcSocket &socket() { return *sock_; }
 
-  void add_callback(const std::string &command, const std::function<void(Client &)> callback);
+  void set_nick_choices(const std::vector<std::string> &choices) {
+    for (const auto &choice : choices) {
+      nick_choices.push(choice);
+    }
+  }
+
+  void add_callback(const std::string &command, const std::function<void(Client &, const Message &)> callback);
 
   bool connect() { return sock_->connect(); }
 
@@ -34,10 +44,11 @@ class Client {
     sock_->write_lines(lines);
   }
 
-  const std::vector<std::function<void(Client &)>> &callbacks_for_command(const std::string &command);
+  const std::vector<std::function<void(Client &, const Message &)>> &callbacks_for_command(const std::string &command);
 
   Message read() {
     auto line = sock_->read_queue().pop();
+    std::cout << "RECV: " << line << std::endl;
     return Message(line);
   }
 };
